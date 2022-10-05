5efc09ba3b9caf74176e4d7144e4f523bcdabe54
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-10-05 17:28:25 +0200

    querydsl 1

--- airport/src/main/java/hu/webuni/airport/repository/FlightRepository.java
+++ airport/src/main/java/hu/webuni/airport/repository/FlightRepository.java
@@ -2,9 +2,12 @@ package hu.webuni.airport.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import hu.webuni.airport.model.Flight;
 
-public interface FlightRepository extends JpaRepository<Flight, Long>, JpaSpecificationExecutor<Flight>{
+public interface FlightRepository extends JpaRepository<Flight, Long>,
+	JpaSpecificationExecutor<Flight>,
+	QuerydslPredicateExecutor<Flight>{
 	
 }
