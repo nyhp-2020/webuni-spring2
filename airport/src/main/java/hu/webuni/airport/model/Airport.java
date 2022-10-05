f8290ad843c34af20f3f992ac448c995fe5a972d
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-10-05 11:24:40 +0200

    airport projekt kezdő állapot

---
+++ airport/src/main/java/hu/webuni/airport/model/Airport.java
@@ -0,0 +1,37 @@
+package hu.webuni.airport.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Airport {
+	
+	@Id
+	@GeneratedValue
+	@EqualsAndHashCode.Include()
+	private long id;
+
+	@Size(min = 3, max = 20)
+	private String name;
+	private String iata;
+	
+	public Airport(String name, String iata) {
+		this.name = name;
+		this.iata = iata;
+	}
+}
---
+++ airport/src/main/java/hu/webuni/airport/service/AirportService.java
@@ -0,0 +1,60 @@
+package hu.webuni.airport.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import hu.webuni.airport.model.Airport;
+import hu.webuni.airport.repository.AirportRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AirportService {
+
+	private final AirportRepository airportRepository;
+
+	@Transactional
+	public Airport save(Airport airport) {
+		checkUniqueIata(airport.getIata(), null);
+		return airportRepository.save(airport);
+	}
+	
+	@Transactional
+	public Airport update(Airport airport) {
+		checkUniqueIata(airport.getIata(), airport.getId());
+		if(airportRepository.existsById(airport.getId())) {
+			return airportRepository.save(airport);
+		}
+		else
+			throw new NoSuchElementException();
+	}
+	
+	private void checkUniqueIata(String iata, Long id) {
+		
+		boolean forUpdate = id != null;
+		Long count = forUpdate ?
+				airportRepository.countByIataAndIdNot(iata, id)
+				:airportRepository.countByIata(iata);
+		
+		if(count > 0)
+			throw new NonUniqueIataException(iata);
+	}
+	
+	public List<Airport> findAll(){
+		return airportRepository.findAll();
+	}
+	
+	public Optional<Airport> findById(long id){
+		return airportRepository.findById(id);
+	}
+	
+	@Transactional
+	public void delete(long id) {
+		airportRepository.deleteById(id);
+	}
+	
+}
