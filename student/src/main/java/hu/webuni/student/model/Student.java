0d259452c5181197d51245b945b771f4790feed8
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-10-10 13:20:27 +0200

    Student

---
+++ student/src/main/java/hu/webuni/student/model/Student.java
@@ -0,0 +1,34 @@
+package hu.webuni.student.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue
+	@EqualsAndHashCode.Include()
+	private long id;
+	
+	private String name;
+	private LocalDate birthdate;
+	private int semester;
+
+}
