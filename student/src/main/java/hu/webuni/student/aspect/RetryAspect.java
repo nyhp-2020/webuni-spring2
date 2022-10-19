aaeeb242d3da2ed04d1515857d828c0cfc0106f0
nyhp2020 <nyhp@freemail.hu>
2022-10-19 21:14:58 +0200

    RetryAspect1

--- student/src/main/java/hu/webuni/student/aspect/RetryAspect.java
+++ student/src/main/java/hu/webuni/student/aspect/RetryAspect.java
@@ -1,14 +1,31 @@
 package hu.webuni.student.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class RetryAspect {
-	
-	public void retryWhenException() {
-		
+
+	@Around("execution(* hu.webuni.student.service.SemesterService.getUsedFreeSemesters(..))")
+	public void retryWhenException(ProceedingJoinPoint joinPoint) throws Throwable {
+		Exception ex = null;
+		int maxret = 5;
+		int waitmillis=500;
+		for (int i = 1; i <= maxret; i++) {
+			try {
+				joinPoint.proceed();
+				return;
+
+			} catch (Exception e) {
+				ex = e;
+				System.out.println("ex" + i);
+				Thread.sleep(waitmillis);
+			}
+		}
+		throw ex;
 	}
 
 }
