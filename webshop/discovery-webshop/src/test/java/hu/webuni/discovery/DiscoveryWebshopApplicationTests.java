186768879098654e0607106c71301853508062ce
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-12-16 13:43:57 +0100

    discovery 2

---
+++ webshop/discovery-webshop/src/test/java/hu/webuni/discovery/DiscoveryWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webuni.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
