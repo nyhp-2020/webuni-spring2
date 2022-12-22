4b4ca5521dfa7ee503cb399be3f700687084445e
MVH04315 <MVH04315@mvh-4501.mvh.local>
2022-12-22 02:00:28 +0100

    gateway-webshop, token-lib

--- webshop/gateway-webshop/src/main/java/hu/webuni/gateway/GatewayWebshopApplication.java
+++ webshop/gateway-webshop/src/main/java/hu/webuni/gateway/GatewayWebshopApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+import hu.webuni.tokenlib.JwtAuthFilter;
+
+//@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication(scanBasePackageClasses = {JwtAuthFilter.class, GatewayWebshopApplication.class})
 public class GatewayWebshopApplication {
 
 	public static void main(String[] args) {
